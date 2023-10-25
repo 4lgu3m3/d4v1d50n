bfbea8f0199046969e710a8bd9c6561003aabc02
4lgu3m3 <127768296+4lgu3m3@users.noreply.github.com>
2023-10-25 12:26:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,13 +3,22 @@ function tocaSom(idElementoAudio){
 }
 const listaDeTeclas = document.querySelectorA11('.tecla');
 
-let contador = 0;
-//Estrutura de repetição - enquanto
- while(contador <listaDeTeclas.leigth){
-      listaDeTeclas[contador].classList[1];
-  listaDeTeclas[contador].onclick = function(){
-tocaSom('#som_tecla_splash');
-      };
-      contador = contador + 1;
-    }
-    console.log(instrumento);
+
+//Estrutura de repetição - Enquanto
+ for( let contador = 0;  contador < listaDeTeclas.leigth; contador++){
+   const tecla = listaDeTeclas[contador];
+   const instrumento = tecla.classlist[1];
+   const idAudio = `#som_${instrumento}`;
+   console.log(idAudio);
+
+   tecla.onclick = function(){
+    tocaSom(idAudio);
+   };
+   tecla.onkeyup = function(){
+     tecla.classlist.add(`ativa`);
+   }
+   tecla.onkeyup = function(){
+    tecla.classlist.remove(`ativa`);
+   }
+      console.log(contador);
+ }
